16e3f106109a8c3c82652182c35ba57da07fe996
aniket <aniketkalbande722@gmail.com.com>
2018-05-19 22:19:59 +1000

    Jamm pR
    Music App

---
+++ src/components/App/App.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+import Spotify from '../../util/Spotify';
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
+    this.state = {
+        searchResults: [],
+        name: 'New Playlist',
+        artist: [],
+        album: [],
+        id: []
+    };
+
+    this.state = {
+      playlistName: '',
+      playlistTracks: {
+        name: [],
+        artist: [],
+        album: [],
+        id: []
+      }
+    };
+
+  }
+  
+  addTrack(track){
+    if(this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)){
+      return ;
+    }
+  }
+
+
+  removeTrack(track){
+    this.state.playlistTracks.filter(savedTrack => savedTrack.id === track.id);
+  }
+
+  updatePlaylistName(name){
+       this.setState({playlistName: name}); 
+  }
+
+  savePlaylist(){
+      const trackURIs = [
+      this.state.playlistTracks
+    ];
+  }
+
+  search(term){
+    Spotify.search(term).then(tracks=>{
+      this.setState({searchResults: tracks});
+    });  
+    console.log({term});
+  }
+  
+  
+  render() {
+    return (
+      <div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App">
+        <SearchBar onSearch = {this.search}/>
+        <div className="App-playlist">
+          <SearchResults searchResults={this.state.searchResults}  onAdd={this.addTrack}/>
+          <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist}/>
+        </div>
+      </div>
+    </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/util/Spotify.js
@@ -0,0 +1,63 @@
+import Track from "../components/Track/Track";
+
+let accessToken = '';
+const clientID ='c0ab0a12fd2b4244a4a56ec8e5887266';
+const redirectedUri = "http://localhost:3000/";
+
+const Spotify = {
+    
+    search(term){
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`,{
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+
+        }).then(response =>{
+            return response.json();
+        }).then(jsonResponse => {
+           if(jsonResponse.tracks){
+               return jsonResponse.Tracks.map(track =>({
+
+                id: track.id,
+                name: track.name,
+                artis: track.artists[0].name,
+                album: track.album.name,
+                uri: track.uri
+
+               }));
+            }
+           }); 
+        },
+    
+    savePlaylist(name,uri){
+        if(name == '' && uri == ''){
+            return;
+        }
+        else{
+            return;
+        }
+    },
+    
+    getAccesToken(){
+        if(accessToken){
+            return accessToken;
+        }
+         
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+        if(accessTokenMatch && expiresInMatch){
+        accessToken = accessTokenMatch[1];
+        const expiresIn = Number(expiresInMatch[1]);
+        window.setTimeout(() => accessToken = '', expiresIn * 1000);
+        window.history.pushState('Access Token', null, '/');
+        return accessToken;
+        }
+        else
+        {
+        const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectedUri}`;
+        window.location = accessUrl;
+        }
+    }
+};
+
+export default Spotify;
